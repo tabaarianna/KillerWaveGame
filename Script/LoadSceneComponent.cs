a246d41d29e4bfb937be594ae5f770a972f6e889
tabaarianna <74322375+tabaarianna@users.noreply.github.com>
2021-04-10 14:19:46 -0700

    Add files via upload

---
+++ Script/LoadSceneComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneComponent : MonoBehaviour 
+{
+ float timer = 0;
+ public string loadThisScene;
+
+ void Start()
+ {  
+     GameManager.Instance.GetComponentInChildren<ScoreManager>().ResetScore();
+ }
+
+ void Update()
+ {
+    timer += Time.deltaTime;
+
+    if (timer > 3)
+    {
+        SceneManager.LoadScene(loadThisScene);
+    }
+ }
+}
---
+++ Script/TitleComponent1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleComponent1 : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("shop");
+        }
+    }
+    void Start()
+    {
+        GameManager.playerLives = 3;
+    }
+
+}
